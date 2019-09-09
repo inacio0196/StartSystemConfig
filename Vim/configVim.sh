ca01ef9509bf473ea517be1bbe1dc32e82658bdd
Rafael <inacio.rafael0196@gmail.com>
2019-09-08 22:34:40 -0300

    .

--- Vim/configVim.sh
+++ Vim/configVim.sh
@@ -21,22 +21,25 @@ echo "Instalando o vim"
 sudo update
 sudo apt install vim 
 
+
+mkdir ~/tmpvim 
+cp .vimrc ~/tmpvim
 cd ~
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-mkdir ~/tmpvim 
-cp .vimrc ~/tmpvim
 cd ~/tmpvim 
 touch ~/.vimrc
-cat .vimrc >> ~/vimrc 
+cat .vimrc >> ~/.vimrc 
+source ~/.vimrc 
 
 echo "Instalando plugins no vim..."
 echo
 echo "Digite dentro do editor de texto Vim no modo Comand ':PlugInstall' e aguarde a instalação dos plugins..."
 echo "Precione [Enter] para entrar no arquivo .vimrc"
+read enter
 vim ~/.vimrc
 
 
