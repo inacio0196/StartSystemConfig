3c58121f61fba6366d3eff5f161497130a8cb653
Rafael <inacio.rafael0196@gmail.com>
2019-09-13 15:46:27 -0300

    .

--- Tools/installTools.sh
+++ Tools/installTools.sh
@@ -3,7 +3,7 @@
 echo "Instalando ferramentas"
 
 sudo apt update
-sudo apt vim net-tools ranger ctorrent blueman pavucontrol 
+sudo apt vim net-tools ranger ctorrent blueman pavucontrol arandr
 
 mkdir ~/Torrents
 
